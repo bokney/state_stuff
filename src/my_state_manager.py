87761c04bce5aa0e804971ab47a4fab5f0da7481
bokney <toknyk@gmail.com>
2024-06-25 09:48:47 +0100

    my_state_manager.py first init

---
+++ src/my_state_manager.py
@@ -0,0 +1,18 @@
+
+class MyState:
+
+    _init_func = None
+    _loop_func = None
+    _exit_func = None
+
+class MyStateManager:
+
+    def __init__(self, initial_state: MyState) -> None:
+        # instance of MyState
+        self._current_state = initial_state
+        assert isinstance(self._current_state, MyState)
+        self._state_step = 0
+
+    def run(self):
+        
+        pass
